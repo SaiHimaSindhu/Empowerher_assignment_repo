6a734b6ed55aba070b1f81f2aab2d475369eed35
SaiHimaSindhu <sindhukancherla2003@gmail.com>
2025-12-09 10:47:57 +0530

    Delete day11_ES6+ Features directory

--- day11_ES6+ Features/Q19/Q10.js
+++
@@ -1,5 +0,0 @@
-const add = (a, b) => a + b;
-
-const isAdult = age => age >= 18;
-
-const double = n => n * 2;
--- day11_ES6+ Features/Q19/Q9.js
+++
@@ -1,8 +0,0 @@
-// Print today’s date using template literal
-console.log(`Today's date is ${new Date().toDateString()}`);
-
-// Print: "Hello NAME, your score is SCORE/100"
-const name = "Alice";
-const score = 86;
-console.log(`Hello ${name}, your score is ${score}/100`);
-
