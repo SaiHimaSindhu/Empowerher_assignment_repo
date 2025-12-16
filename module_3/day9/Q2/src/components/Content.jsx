8463e77ca0bbfbb469d0b528523aeb381026f31f
SaiHimaSindhu <sindhukancherla2003@gmail.com>
2025-12-16 12:44:44 +0530

    Created new react app

---
+++ module_3/day9/Q2/src/components/Content.jsx
@@ -0,0 +1,15 @@
+function Content() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "40px",
+        fontSize: "20px",
+      }}
+    >
+      This is Main Content
+    </div>
+  );
+}
+
+export default Content;
---
+++ module_3/day9/Q2/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f1f1f1",
+        padding: "10px",
+        fontSize: "14px",
+        color: "#555",
+        textAlign: "center",
+      }}
+    >
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
