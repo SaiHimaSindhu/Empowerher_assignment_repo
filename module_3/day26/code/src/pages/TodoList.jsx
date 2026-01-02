1544c9320c86d35595d576694df225929f318dec
SaiHimaSindhu <sindhukancherla2003@gmail.com>
2026-01-02 10:50:12 +0530

    Task completed

---
+++ module_3/day26/code/src/pages/TodoList.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getTodos } from "../api/todoService";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  }, []);
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <Link to={`/todo/${todo.id}`}>
+              {todo.title} - {todo.completed ? "Completed" : "Pending"}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
